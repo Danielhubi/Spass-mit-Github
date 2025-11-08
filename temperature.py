ad73839936ee78922ca4a864a7f041f1e45df810
Danielhubi <773400@fom-net.de>
2025-11-08 13:53:25 +0100

    Update temperature.py
    Durchschittstemperatur, Min und max werte, Text ausgabebei bestimmten Temperaturen

--- temperature.py
+++ temperature.py
@@ -1,1 +1,17 @@
+#Temperaturen auswerten
 
+temperature = [14, 18, 21, 20, 19, 16, 15]
+
+average = sum(temperature) / len(temperature)
+print("Durchschnittstemperatur:", average)
+
+print("Höchste Temperatur:", max(temperature))
+print("Niedrigste Temperatur:", min(temperature))
+
+def check_temperature(temperatures):
+    average = sum(temperatures) / len(temperatures)
+    
+    if average > 20:
+        print("Woche war warm!")
+    else:
+        print("Woche war eher kühl")
