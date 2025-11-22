9b1a909ec340e47e7c61d3ecbbd450791037b3bc
Danielhubi <773400@fom-net.de>
2025-11-22 14:08:10 +0100

    Add files via upload

---
+++ start.py
@@ -0,0 +1,18 @@
+# Der "Feed" – eine Liste von Social-Media-Posts (als Strings)
+# Jeder Post ist ein String in unserer Liste.
+feed_data = [
+    "Hallo Welt! Mein erstes Python-Projekt. #python #neu",
+    "Ich liebe Listen! Listen sind so vielseitig. #python",
+    "Was sind eigentlich Seiteneffekte? #frage",
+    "SPAM! Kaufen Sie jetzt! Billig! SPAM!",
+    "Sets sind super für einzigartige Elemente. #tipp",
+    "Python ist nicht nur eine Schlange. @PythonUser123",
+    "Wer hat Lust auf ein weiteres Projekt? #python",
+    "Dieser Post ist nur Fülltext.",
+    "SPAM! Nicht klicken! SPAM!",
+    "Ich mag Tupel, weil sie unveränderlich sind.",
+    "Hallo nochmal, Welt! @PythonUser123"
+]
+
+# Eine Liste von Wörtern, die moderiert werden sollen
+forbidden_words = ["SPAM", "Billig", "kaufen"]
